657c7cc48ad5c7681a5e2aea22cb31ceb2a456b4
sarinbeam30 <45058857+sarinbeam30@users.noreply.github.com>
2021-03-01 01:23:15 +0700

    add TestWebSocket.js

---
+++ TestWebSocket.js
@@ -0,0 +1,20 @@
+const WebSocket = require('ws');
+const Window = require('window');
+ 
+const window = new Window();
+
+const socket = new WebSocket(
+    'ws://'
+    + '127.0.0.1:9000'
+    + '/ws/realtimeupdate'
+    + '/'
+);
+
+socket.onmessage = function(e) {
+    const data = JSON.parse(e.data);
+    console.log("DATA : " + e.data + '\n');
+}
+
+socket.onclose = function(e) {
+    console.error('Chat socket closed unexpectedly');
+};
